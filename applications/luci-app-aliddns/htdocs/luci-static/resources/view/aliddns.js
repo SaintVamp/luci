5d787d23b1d532525fc9979aadaa7b148e93db24
admin <saint-wp@163.com>
2023-09-14 00:00:27 +0800

    修改aliddns

--- applications/luci-app-aliddns/htdocs/luci-static/resources/view/aliddns.js
+++ applications/luci-app-aliddns/htdocs/luci-static/resources/view/aliddns.js
@@ -4,31 +4,29 @@
 'require uci';
 
 return view.extend({
-	load: function() {
-		return Promise.all([
-			uci.load('aliddns')
-		]);
-	},
+    load: function () {
+        return Promise.all([
+            uci.load('aliddns')
+        ]);
+    },
 
-	render: function(data) {
+    render: function (data) {
 
-		var m, s, o;
+        var m, s, o;
 
-		m = new form.Map('aliddns', [_('阿里云ddns工具')],
-			_('阿里云ddns工具'));
+        m = new form.Map('aliddns', [_('阿里云ddns工具')]);
 
-		s = m.section(form.TypedSection, 'alikey', 'aliddns', _('基础设置'));
-		s.addremove = false;
+        s = m.section(form.TypedSection, 'alikey', _('基础设置'));
+        s.addremove = false;
 
-		o = s.option(form.Value, "AccessKeyId", _("阿里云接入key"),
-			_("写接入key."));
-		o.rmempty = false;
+        o = s.option(form.Value, "AccessKeyId", _("阿里云接入key"),
+            _("写接入key."));
+        o.rmempty = false;
 
-		o = s.option(form.Value, "AccessKeySecret", _("阿里云接入密码"),
-			_("写接入密码."));
-		o.rmempty = false;
+        o = s.option(form.Value, "AccessKeySecret", _("阿里云接入密码"),
+            _("写接入密码."));
+        o.rmempty = false;
 
-
-		return m.render();
-	}
+        return m.render();
+    }
 });
