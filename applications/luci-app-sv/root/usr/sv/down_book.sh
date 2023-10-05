332470d5d5744942091a1af78013fc4461483b2d
admin <saint-wp@163.com>
2023-10-06 01:16:56 +0800

    更新下载图书

--- applications/luci-app-sv/root/usr/sv/down_book.sh
+++ applications/luci-app-sv/root/usr/sv/down_book.sh
@@ -1,4 +1,4 @@
 mkdir /usr/sv/out
-curl -o /usr/sv/py_book.py https://raw.githubusercontent.com/SaintVamp/py_book/master/run.py
-curl -o /usr/sv/book_urls.txt https://raw.githubusercontent.com/SaintVamp/py_book/master/book_urls.txt
+curl -o /usr/sv/py_book.py https://gitee.com/saintvamp/py_book/raw/master/run.py
+curl -o /usr/sv/book_urls.txt https://gitee.com/saintvamp/py_book/raw/master/book_urls.txt
 nohup python /usr/sv/py_book.py > /usr/sv/out/run.log 2>&1 &
