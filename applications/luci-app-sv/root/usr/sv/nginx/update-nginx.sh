64160e075230111e0f99309c5ed2cfe9f40fda6b
Charlie <saint-wp@163.com>
2023-10-14 15:50:15 +0800

    新增全局依赖luci-app-all

--- applications/luci-app-sv/root/usr/sv/nginx/update-nginx.sh
+++ applications/luci-app-sv/root/usr/sv/nginx/update-nginx.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
-echo "please open backup-port to visit luci or ssh!!!"
-echo "please open backup-port to visit luci or ssh!!!"
-echo "please open backup-port to visit luci or ssh!!!"
-echo "after 10 sec ,run script."
-sleep 1
-echo "after 9 sec ,run script."
-sleep 1
-echo "after 8 sec ,run script."
-sleep 1
-echo "after 7 sec ,run script."
-sleep 1
-echo "after 6 sec ,run script."
-sleep 1
-echo "after 5 sec ,run script."
-sleep 1
-echo "after 4 sec ,run script."
-sleep 1
-echo "after 3 sec ,run script."
-sleep 1
-echo "after 2 sec ,run script."
-sleep 1
-echo "after 1 sec ,run script."
-sleep 1
-
+if [ ! $1 ]
+then
+    echo "please open backup-port to visit luci or ssh!!!"
+    echo "please open backup-port to visit luci or ssh!!!"
+    echo "please open backup-port to visit luci or ssh!!!"
+    echo "after 10 sec ,run script."
+    sleep 1
+    echo "after 9 sec ,run script."
+    sleep 1
+    echo "after 8 sec ,run script."
+    sleep 1
+    echo "after 7 sec ,run script."
+    sleep 1
+    echo "after 6 sec ,run script."
+    sleep 1
+    echo "after 5 sec ,run script."
+    sleep 1
+    echo "after 4 sec ,run script."
+    sleep 1
+    echo "after 3 sec ,run script."
+    sleep 1
+    echo "after 2 sec ,run script."
+    sleep 1
+    echo "after 1 sec ,run script."
+    sleep 1
+fi
 hostname=`uci get system.@system[0].hostname`
 if [ $hostname = 'R404' ]; then
     curl -o /etc/nginx/nginx.conf https://gitee.com/saintvamp/nginx_conf/raw/master/nginx-404.conf
