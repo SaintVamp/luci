ced7eda7a50c4aec5fe615843b115de3a696cee2
admin <saint-wp@163.com>
2023-10-19 16:47:46 +0800

    update

--- applications/luci-app-aliddns/root/usr/ddns/ddns.sh
+++ applications/luci-app-aliddns/root/usr/ddns/ddns.sh
@@ -312,12 +312,17 @@ fi
 echo "start update ddns..."
 
 #add support */%2A and @/%40 record
+hostname=$(uci get system.@system[0].hostname)
+if [ "$hostname" = "R404" ]; then
+    url_name="4.0.4.51:8080"
+elif [ "$hostname" = "R2804" ]; then
+    url_name="tomcat.404.svsoft.fun:404"
+fi
 if [ "$ali_ddns_record_id" = "" ]
 then
     echo "add record starting"
     ali_ddns_record_id=$(add_record | get_record_id)
-    curl -s "http://4.0.4.51:8080/Serv/ddns?domain=$ali_ddns_name&ip=$(enc $machine_ip)"
-    curl -s "https://tomcat.404.svsoft.fun:404/Serv/ddns?domain=$ali_ddns_name&ip=$(enc $machine_ip)"
+    curl -s "http://$url_name/Serv/ddns?domain=$ali_ddns_name&ip=$(enc $machine_ip)"
     if [ "$ali_ddns_record_id" = "" ]
     then
         ehco "ali_ddns_record_id is empty."
@@ -333,7 +338,6 @@ then
 else
     echo "update record starting"
     update_record "$ali_ddns_record_id"
-    curl -s "http://4.0.4.51:8080/Serv/ddns?domain=$ali_ddns_name&ip=$(enc $machine_ip)"
-    curl -s "https://tomcat.404.svsoft.fun:404/Serv/ddns?domain=$ali_ddns_name&ip=$(enc $machine_ip)"
+    curl -s "http://$url_name/Serv/ddns?domain=$ali_ddns_name&ip=$(enc $machine_ip)"
     echo "updated record id is:" "$ali_ddns_record_id"
 fi
