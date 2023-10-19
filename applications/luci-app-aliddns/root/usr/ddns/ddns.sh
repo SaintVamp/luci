ce33ac4f34118049919fae69e2744f34ccdd06f9
admin <saint-wp@163.com>
2023-10-19 16:44:10 +0800

    update

--- applications/luci-app-aliddns/root/usr/ddns/ddns.sh
+++ applications/luci-app-aliddns/root/usr/ddns/ddns.sh
@@ -5,7 +5,7 @@ set -e
 # 功能：用于更新阿里云域名IP，实现DDNS功能
 #
 # 在 http://www.gebi1.com/forum.php?mod=viewthread&tid=287344&page=1&_dsign=8f94f74c 提供的脚本文件基础上修改的。
-# ghui, modified 12/2/2019
+# modified 12/2/2019
 # 在 N1 debian Buster with Armbian Linux 5.3.0-aml-g12 手动执行/定时任务(crontab)执行测试通过
 #================================================================================================================#
 #
@@ -13,22 +13,22 @@ set -e
 #
 # 方法1. 外部参数
 # 修改源码，将对应参数 修改为$1,$2,$3,$4,$5,$6
-# aliddns.sh <AccessKeyId> <AccessKeySecret> <aliddns_subdomain> <aliddns_domain> <aliddns_iptype> <aliddns_ttl>
+# ali_ddns.sh <AccessKeyId> <AccessKeySecret> <ali_ddns_subdomain> <ali_ddns_domain> <ali_ddns_ip_type> <ali_ddns_ttl>
 # 示例（A 代表 IPv4，AAAA 代表 IPv6）:
-# 执行：aliddns.sh "xxxx" "xxx" "test" "mydomain.site" "A" 600
-# 执行：aliddns.sh "xxxx" "xxx" "test" "mydomain.site" "AAAA" 600
+# 执行：ali_ddns.sh "xxx" "xxx" "test" "my_domain.site" "A" 600
+# 执行：ali_ddns.sh "xxx" "xxx" "test" "my_domain.site" "AAAA" 600
 #
 # 方法2. 内部参数
 # 修改源码，将$1,$2,$3,$4,$5,$6 替换为对应参数
 #
 # 示例:
-# AccessKeyId="xxxx"
+# AccessKeyId="xxx"
 # AccessKeySecret="xxx"
-# aliddns_subdomain="test"
-# aliddns_domain="mydomain.site"
-# aliddns_iptype="A"
-# aliddns_ttl=600
-# 执行：aliddns.sh
+# ali_ddns_subdomain="test"
+# ali_ddns_domain="my_domain.site"
+# ali_ddns_ip_type="A"
+# ali_ddns_ttl=600
+# 执行：ali_ddns.sh
 #
 #================================================================================================================#
 
@@ -42,15 +42,15 @@ AccessKeySecret=$2
 
 
 
-# (*)域名：test.mydomain.com
-aliddns_subdomain=$3 #'test'
-aliddns_domain=$4 #'mydomain.com'
+# (*)域名：test.my_domain.com
+ali_ddns_subdomain=$3 #'test'
+ali_ddns_domain=$4 #'my_domain.com'
 
 # (*)ip地址类型：'A' 或 'AAAA'，代表ipv4 和 ipv6
-aliddns_iptype=$5 # 'A' 或 'AAAA'，代表ipv4 和 ipv6
+ali_ddns_ip_type=$5 # 'A' 或 'AAAA'，代表ipv4 和 ipv6
 
 # TTL 默认10分钟 = 600秒
-aliddns_ttl=$6 #"600"
+ali_ddns_ttl=$6 #"600"
 
 dns_server=$7
 
@@ -60,35 +60,35 @@ dns_server=$7
 
 machine_ip=""
 ddns_ip=""
-aliddns_record_id=""
+ali_ddns_record_id=""
 
-if [ "$aliddns_subdomain" = "@" ]
+if [ "$ali_ddns_subdomain" = "@" ]
 then
-  aliddns_name=$aliddns_domain
+  ali_ddns_name=$ali_ddns_domain
 else
-  aliddns_name=$aliddns_subdomain.$aliddns_domain
+  ali_ddns_name=$ali_ddns_subdomain.$ali_ddns_domain
 fi
-now=`date`
+now=$(date)
 echo "**************************************************"
 echo "$now"
-echo "$aliddns_name"
-echo "$aliddns_iptype"
+echo "$ali_ddns_name"
+echo "$ali_ddns_ip_type"
 echo "--------------------"
 function getMachine_IPv4() {
-    echo $(/usr/bin/wget -qO- -t1 -T2 http://4.ipw.cn)
+    /usr/bin/wget -qO- -t1 -T2 http://4.ipw.cn
 }
 function getMachine_IPv42() {
-    echo $(/usr/bin/wget -qO- -t1 -T2 https://ipv4.netarm.com/)
+    /usr/bin/wget -qO- -t1 -T2 https://ipv4.netarm.com/
 }
 function getMachine_IPv6() {
-    echo $(/usr/bin/wget -qO- -t1 -T2 http://6.ipw.cn)
+    /usr/bin/wget -qO- -t1 -T2 http://6.ipw.cn
 }
 function getMachine_IPv62() {
-    echo $(/usr/bin/wget -qO- -t1 -T2 https://ipv6.netarm.com/)
+    /usr/bin/wget -qO- -t1 -T2 https://ipv6.netarm.com/
 }
 function getDDNS_IP() {
-    current_ip=`nslookup -query=$aliddns_iptype $aliddns_name $dns_server| grep "Address" | grep -v "#53" | grep -v ":53" | awk '{print $2}'`
-    echo $current_ip
+    current_ip=$(nslookup -query="$ali_ddns_ip_type" "$ali_ddns_name" "$dns_server"| grep "Address" | grep -v "#53" | grep -v ":53" | awk '{print $2}')
+    echo "$current_ip"
 }
 function getJsonValuesByAwk() {
     awk -v json="$1" -v key="$2" -v defaultValue="$3" 'BEGIN{
@@ -135,8 +135,8 @@ function getJsonValuesByAwk() {
         }
     }'
 }
-function urlencode() {
-    # urlencode <string>
+function url_encode() {
+    # url_encode <string>
     out=""
     while read -n1 c
     do
@@ -145,146 +145,148 @@ function urlencode() {
             *) out="$out`printf '%%%02X' "'$c"`" ;;
         esac
     done
-    echo -n $out
+    echo -n "$out"
 }
 function enc() {
-    echo -n "$1" | urlencode
+    echo -n "$1" | url_encode
 }
 function send_request() {
-    local args="AccessKeyId=$AccessKeyId&Action=$1&Format=json&$2&Version=2015-01-09"
-    local hash=$(echo -n "GET&%2F&$(enc "$args")" | openssl dgst -sha1 -hmac "$AccessKeySecret&" -binary | openssl base64)
+    args="AccessKeyId=$AccessKeyId&Action=$1&Format=json&$2&Version=2015-01-09"
+    hash=$(echo -n "GET&%2F&$(enc "$args")" | openssl dgst -sha1 -hmac "$AccessKeySecret&" -binary | openssl base64)
     curl -s "http://alidns.aliyuncs.com/?$args&Signature=$(enc "$hash")"
 }
-function get_recordid() {
+function get_record_id() {
     grep -Eo '"RecordId":"[0-9]+"' | cut -d':' -f2 | tr -d '"'
 }
-function query_recordid() {
-    timestamp=`date -u "+%Y-%m-%dT%H%%3A%M%%3A%SZ"`
-    send_request "DescribeSubDomainRecords" "SignatureMethod=HMAC-SHA1&SignatureNonce=$timestamp&SignatureVersion=1.0&SubDomain=$aliddns_name&Timestamp=$timestamp&Type=$aliddns_iptype"
+function query_record_id() {
+    timestamp=$(date -u "+%Y-%m-%dT%H%%3A%M%%3A%SZ")
+    send_request "DescribeSubDomainRecords" "SignatureMethod=HMAC-SHA1&SignatureNonce=$timestamp&SignatureVersion=1.0&SubDomain=$ali_ddns_name&Timestamp=$timestamp&Type=$ali_ddns_ip_type"
 }
 function update_record() {
-    timestamp=`date -u "+%Y-%m-%dT%H%%3A%M%%3A%SZ"`
-    send_request "UpdateDomainRecord" "RR=$aliddns_subdomain&RecordId=$1&SignatureMethod=HMAC-SHA1&SignatureNonce=$timestamp&SignatureVersion=1.0&TTL=$aliddns_ttl&Timestamp=$timestamp&Type=$aliddns_iptype&Value=$(enc $machine_ip)"
+    timestamp=$(date -u "+%Y-%m-%dT%H%%3A%M%%3A%SZ")
+    send_request "UpdateDomainRecord" "RR=$ali_ddns_subdomain&RecordId=$1&SignatureMethod=HMAC-SHA1&SignatureNonce=$timestamp&SignatureVersion=1.0&TTL=$ali_ddns_ttl&Timestamp=$timestamp&Type=$ali_ddns_ip_type&Value=$(enc "$machine_ip")"
 }
 function delete_record() {
-    timestamp=`date -u "+%Y-%m-%dT%H%%3A%M%%3A%SZ"`
-    send_request "DeleteDomainRecord" "RR=$aliddns_subdomain&RecordId=$1&SignatureMethod=HMAC-SHA1&SignatureNonce=$timestamp&SignatureVersion=1.0&TTL=$aliddns_ttl&Timestamp=$timestamp&Type=$aliddns_iptype&Value=$(enc $machine_ip)"
+    timestamp=$(date -u "+%Y-%m-%dT%H%%3A%M%%3A%SZ")
+    send_request "DeleteDomainRecord" "RR=$ali_ddns_subdomain&RecordId=$1&SignatureMethod=HMAC-SHA1&SignatureNonce=$timestamp&SignatureVersion=1.0&TTL=$ali_ddns_ttl&Timestamp=$timestamp&Type=$ali_ddns_ip_type&Value=$(enc "$machine_ip")"
 }
 function add_record() {
     echo "add"
-    timestamp=`date -u "+%Y-%m-%dT%H%%3A%M%%3A%SZ"`
-    send_request "AddDomainRecord&DomainName=$aliddns_domain" "RR=$aliddns_subdomain&SignatureMethod=HMAC-SHA1&SignatureNonce=$timestamp&SignatureVersion=1.0&TTL=$aliddns_ttl&Timestamp=$timestamp&Type=$aliddns_iptype&Value=$(enc $machine_ip)"
+    timestamp=$(date -u "+%Y-%m-%dT%H%%3A%M%%3A%SZ")
+    # shellcheck disable=SC2086
+    send_request "AddDomainRecord&DomainName=$ali_ddns_domain" "RR=$ali_ddns_subdomain&SignatureMethod=HMAC-SHA1&SignatureNonce=$timestamp&SignatureVersion=1.0&TTL=$ali_ddns_ttl&Timestamp=$timestamp&Type=$ali_ddns_ip_type&Value=$(enc $machine_ip)"
 }
-if [ "$aliddns_record_id" = "" ]
+if [ "$ali_ddns_record_id" = "" ]
 then
-    aliddns_record_id=`query_recordid`
-#    echo "---------aliddns_record_id-------" $aliddns_record_id "\n"
-    recordid_num=`getJsonValuesByAwk "$aliddns_record_id" "TotalCount" "defaultValue"`
-    recordids=`getJsonValuesByAwk "$aliddns_record_id" "RecordId" "defaultValue" | tr -d '\n'`
-    recordids=${recordids//\"\"/\" \"}
-    recordids=${recordids//\"/}
+    ali_ddns_record_id=$(query_record_id)
+#    echo "---------ali_ddns_record_id-------" $ali_ddns_record_id "\n"
+    record_id_num=$(getJsonValuesByAwk "$ali_ddns_record_id" "TotalCount" "defaultValue")
+    record_ids=$(getJsonValuesByAwk "$ali_ddns_record_id" "RecordId" "defaultValue" | tr -d '\n')
+    record_ids=${record_ids//\"\"/\" \"}
+    record_ids=${record_ids//\"/}
     sleep 2
-    if [ $recordid_num -gt 1 ]
+    if [ $((record_id_num)) -gt 1 ]
     then
-        str1=`echo $recordids | awk '{print $1}'`
-        str2=`echo $recordids | awk '{print $2}'`
-        str3=`echo $recordids | awk '{print $3}'`
-        str4=`echo $recordids | awk '{print $1}'`
-        str5=`echo $recordids | awk '{print $2}'`
-        str6=`echo $recordids | awk '{print $3}'`
-        str7=`echo $recordids | awk '{print $3}'`
-        str8=`echo $recordids | awk '{print $1}'`
-        str9=`echo $recordids | awk '{print $2}'`
-        str10=`echo $recordids | awk '{print $3}'`
+        str1=$(echo "$record_ids" | awk '{print $1}')
+        str2=$(echo "$record_ids" | awk '{print $2}')
+        str3=$(echo "$record_ids" | awk '{print $3}')
+        str4=$(echo "$record_ids" | awk '{print $1}')
+        str5=$(echo "$record_ids" | awk '{print $2}')
+        str6=$(echo "$record_ids" | awk '{print $3}')
+        str7=$(echo "$record_ids" | awk '{print $3}')
+        str8=$(echo "$record_ids" | awk '{print $1}')
+        str9=$(echo "$record_ids" | awk '{print $2}')
+        str10=$(echo "$record_ids" | awk '{print $3}')
         if [ -n "$str1" ]
         then
-            delete_record $str1
+            delete_record "$str1"
             sleep 5
         fi
         if [ -n "$str2" ]
         then
-            delete_record $str2
+            delete_record "$str2"
             sleep 5
         fi
         if [ -n "$str3" ]
         then
-            delete_record $str3
+            delete_record "$str3"
             sleep 5
         fi
         if [ -n "$str4" ]
         then
-            delete_record $str4
+            delete_record "$str4"
             sleep 5
         fi
         if [ -n "$str5" ]
         then
-            delete_record $str5
+            delete_record "$str5"
             sleep 5
         fi
         if [ -n "$str6" ]
         then
-            delete_record $str6
+            delete_record "$str6"
             sleep 5
         fi
         if [ -n "$str7" ]
         then
-            delete_record $str7
+            delete_record "$str7"
             sleep 5
         fi
         if [ -n "$str8" ]
         then
-            delete_record $str8
+            delete_record "$str8"
             sleep 5
         fi
         if [ -n "$str9" ]
         then
-            delete_record $str9
+            delete_record "$str9"
             sleep 5
         fi
         if [ -n "$str10" ]
         then
-            delete_record $str10
+            delete_record "$str10"
             sleep 5
         fi
     else
-        aliddns_record_id=`query_recordid | get_recordid`
-        echo "----------------" $aliddns_record_id "\n"
-        if [ "$aliddns_iptype" = 'A' ]
+        ali_ddns_record_id=$(query_record_id | get_record_id)
+        echo "ali_ddns_record_id is " "$ali_ddns_record_id"
+        echo "****************"
+        if [ "$ali_ddns_ip_type" = 'A' ]
         then
-            aliddnsipv4_record_id=$aliddns_record_id
+            ali_ddns_ipv4_record_id=$ali_ddns_record_id
         else
-            aliddnsipv6_record_id=$aliddns_record_id
+            ali_ddns_ipv6_record_id=$ali_ddns_record_id
         fi
     fi
 fi
 
-ddns_ip=`echo "$(getDDNS_IP)"`
+ddns_ip=getDDNS_IP
 echo "ddns_ip = $ddns_ip"
-if [ "$aliddns_iptype" = 'A' ]
+if [ "$ali_ddns_ip_type" = 'A' ]
 then
     echo "ddns is IPv4."
-    machine_ip=`echo "$(getMachine_IPv4)"`
+    machine_ip=getMachine_IPv4
     if [ "$machine_ip" = "" ]
     then
-        machine_ip=`echo "$(getMachine_IPv42)"`
+        machine_ip=getMachine_IPv42
     fi
     echo "machine_ip = $machine_ip"
-    aliddns_record_id=$aliddnsipv4_record_id
-    exist_local=`ifconfig | grep "inet addr"| grep "inet addr:$machine_ip  P"| wc -l`
-    exist_ddns=`echo $ddns_ip | grep $machine_ip | wc -l`
-    exist_ddns_local=`ifconfig | grep "inet addr"| grep "inet addr:$ddns_ip  P"| wc -l`
+    ali_ddns_record_id=$ali_ddns_ipv4_record_id
+    exist_local=$(ip addr show pppoe-wan | grep "scope global pppoe-wan" | grep -c "$machine_ip")
+    exist_ddns=$(echo "$ddns_ip" | grep -c "$machine_ip")
+    exist_ddns_local=$(ip addr show pppoe-wan | grep "scope global pppoe-wan" | grep -c "$ddns_ip")
 else
     echo "ddns is IPv6."
-    machine_ip=`echo "$(getMachine_IPv6)"`
+    machine_ip=getMachine_IPv6
     if [ "$machine_ip" = "" ]
     then
-        machine_ip=`echo "$(getMachine_IPv62)"`
+        machine_ip=getMachine_IPv62
     fi
     echo "machine_ip = $machine_ip"
-    aliddns_record_id=$aliddnsipv6_record_id
-    exist_local=`ifconfig | grep "inet6 addr"| grep "inet6 addr: $machine_ip/"| wc -l`
-    exist_ddns=`echo $ddns_ip | grep $machine_ip | wc -l`
-    exist_ddns_local=`ifconfig | grep "inet6 addr"| grep "inet6 addr: $ddns_ip/"| wc -l`
+    ali_ddns_record_id=$ali_ddns_ipv6_record_id
+    exist_local=$(ip addr show br-lan | grep "scope global dynamic noprefixroute" | grep -c $machine_ip)
+    exist_ddns=$(echo $ddns_ip | grep -c $machine_ip )
+    exist_ddns_local=$(ip addr show br-lan | grep "scope global dynamic noprefixroute" | grep -c $ddns_ip)
 fi
 
 if [ "$machine_ip" = "" ]
@@ -292,46 +294,46 @@ then
     echo "machine_ip is empty!"
     exit 0
 fi
-if [ $exist_local -eq 0 ]
+if [ $((exist_local)) -eq 0 ]
 then
     echo "machine_ip is error"
 fi
-if [ $exist_ddns -gt 0 ]
+if [ $((exist_ddns)) -gt 0 ]
 then
-    echo "skipping ddns \n"
+    print "skipping ddns \n"
     exit 1
 else
-    if [ $exist_ddns_local -gt 0 ]
+    if [ $((exist_ddns_local)) -gt 0 ]
     then
-        echo "skipping ddns_local \n"
+        print "skipping ddns_local \n"
         exit 1
     fi
 fi
 echo "start update ddns..."
 
 #add support */%2A and @/%40 record
-if [ "$aliddns_record_id" = "" ]
+if [ "$ali_ddns_record_id" = "" ]
 then
     echo "add record starting"
-    aliddns_record_id=`add_record | get_recordid`
-    curl -s "http://4.0.4.51:8080/Serv/ddns?domain=$aliddns_name&ip=$(enc $machine_ip)"
-    curl -s "https://tomcat.404.svsoft.fun:404/Serv/ddns?domain=$aliddns_name&ip=$(enc $machine_ip)"
-    if [ "$aliddns_record_id" = "" ]
+    ali_ddns_record_id=$(add_record | get_record_id)
+    curl -s "http://4.0.4.51:8080/Serv/ddns?domain=$ali_ddns_name&ip=$(enc $machine_ip)"
+    curl -s "https://tomcat.404.svsoft.fun:404/Serv/ddns?domain=$ali_ddns_name&ip=$(enc $machine_ip)"
+    if [ "$ali_ddns_record_id" = "" ]
     then
-        echo "aliddns_record_id is empty. \n"
+        ehco "ali_ddns_record_id is empty."
     else
-        if [ "$aliddns_iptype" = 'A' ]
+        if [ "$ali_ddns_ip_type" = 'A' ]
         then
-            aliddnsipv4_record_id=$aliddns_record_id
+            ali_ddns_ipv4_record_id=$ali_ddns_record_id
         else
-            aliddnsipv6_record_id=$aliddns_record_id
+            ali_ddns_ipv6_record_id=$ali_ddns_record_id
         fi
-        echo "added record $aliddns_record_id \n"
+        echo "added record id is:" "$ali_ddns_record_id"
     fi
 else
     echo "update record starting"
-    update_record $aliddns_record_id
-    curl -s "http://4.0.4.51:8080/Serv/ddns?domain=$aliddns_name&ip=$(enc $machine_ip)"
-    curl -s "https://tomcat.404.svsoft.fun:404/Serv/ddns?domain=$aliddns_name&ip=$(enc $machine_ip)"
-    echo "updated record $aliddns_record_id \n"
+    update_record "$ali_ddns_record_id"
+    curl -s "http://4.0.4.51:8080/Serv/ddns?domain=$ali_ddns_name&ip=$(enc $machine_ip)"
+    curl -s "https://tomcat.404.svsoft.fun:404/Serv/ddns?domain=$ali_ddns_name&ip=$(enc $machine_ip)"
+    echo "updated record id is:" "$ali_ddns_record_id"
 fi
